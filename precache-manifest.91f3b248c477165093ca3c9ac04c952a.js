c3689fabc3f31f489b38566118388a82dd344f29
Rob Martin <rmartin3@alumni.unca.edu>
2019-10-17 15:16:33 -0700

    Updates

---
+++ precache-manifest.91f3b248c477165093ca3c9ac04c952a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c520f02cdc35f6739446393232f609f",
+    "url": "/bulk-record-create/index.html"
+  },
+  {
+    "revision": "9329817910974f832064",
+    "url": "/bulk-record-create/static/css/main.81730de2.chunk.css"
+  },
+  {
+    "revision": "22c138330988d59eac7d",
+    "url": "/bulk-record-create/static/js/2.cf44d153.chunk.js"
+  },
+  {
+    "revision": "9329817910974f832064",
+    "url": "/bulk-record-create/static/js/main.92eb41cd.chunk.js"
+  },
+  {
+    "revision": "5303a6724394e9650e15",
+    "url": "/bulk-record-create/static/js/runtime-main.254e62a0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/bulk-record-create/precache-manifest.91f3b248c477165093ca3c9ac04c952a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bulk-record-create/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
